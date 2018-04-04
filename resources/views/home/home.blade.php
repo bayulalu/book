b6305a8c65dde9066005c0f524d47328fa411ef1
Bayu lalu <bayulalu3@gmail.com>
2018-04-05 06:58:24 +0700

    halaman utama

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,7 +5,32 @@
 </head>
 <link rel="stylesheet" type="text/css" href="/css/materialize.min.css">
 <link rel="stylesheet" type="text/css" href="/css/home.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <body>
+<!-- nav -->
+<!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content katagori">
+  <li><a href="#!">Buku Belajar</a></li>
+  <li class="divider"></li>
+  <li ><a href="#!">Novel</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">Motifasi</a></li>
+</ul>
+
+<div class="navbar-fixed nav-menu">
+<nav>
+
+  <div class="nav-wrapper nav-color">
+    <a href="#!" class="brand-logo logo">Buku</a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="sass.html">Home</a></li>
+      <li><a href="badges.html">Buku</a></li>
+      <!-- Dropdown Trigger -->
+      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Katagori<i class="material-icons right">arrow_drop_down</i></a></li>
+    </ul>
+  </div>
+</nav>
+</div>
 	<div id="nav-bg">
 		<div class="row">
 			<div class="col s6">
@@ -13,21 +38,156 @@
 			 </div>
 
 		 <div  class="col s6 ">
-			<a href="" class="waves-effect waves-ligh right btn novel">Novel</a>
-			<a href="" class=" waves-effect waves-ligh right btn home">Home</a>
+			<a href="" class="waves-effect waves-ligh right btn novel">Register</a>
+			<a href="" class=" waves-effect waves-ligh right btn home">Daftar</a>
 		</div>
 		
 	</div>
 
 	<div class="container">
-		<div>
-			{{-- <h3>Membaca Adalah Yang Guru Terbaik</h3> --}}
-			<h3>Ayo Membaca</h3>
-			<p>Ilmu Adalah Insevestasi Berharga Untuk Masa Depan</p>
+		<div class="nav-conten" >
+			<h3 >Reading Is The Best Teacher </h3>
+			<span class="center " id="baca"></span>
+			
 		</div>
 		
 	</div>
+</div>
+<!-- conten -->
+<div class="row con">
+	<div class="col s12">
+			
+		<div class="container center lorem">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut 
+		</div>
+	</div>
+</div>
 
+<div class="container ">
+	<h4 class="center title">Membaca Buku</h4>
+	<div class="row">
+		<div class="col s4 gambar-kiri" >
+			<img src="img/belajar-sabah.png" class="right">
+		</div>
+
+		<div class="col s6 offset-s2 konten-kanan">
+			<h5 ><b>Membeaca </b></h5>
+			<p>Membaca Adalah merupakan kegiatan memahami teks bacaan dengan tujuan untuk memperoleh informasi dari teks yang kita baca. Pada saat membaca, biasanya dalam teks bacaan yang kita baca terkandung makna yang tersirat (makna yang tersembunyi) dan tersurat (makna yang tertulis)</p>
+		</div>
+
+	</div>
+</div>
+
+<div class="wrap-sp center">
+	<h4>Lorem ipsum dolor sit </h4>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+</div>
+
+<div class="container wrap-requered">
+	<div class="row" >
+		<div class="col s6">
+		<h5 class="center">Requeres Book</h5>
+			<div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="first_name" type="text" class="validate">
+          <label for="first_name">Title</label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Subject</label>
+        </div>
+      </div>
+
+      <button class="btn right request">Requeres</button>
+     
+      </form>
+  </div>
+        
+		</div>
+
+		<div class="col s6">
+			<!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.5419692963987!2d116.41050811413017!3d-8.63989239024054!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcdb4a950743809%3A0xbfc89d26bf22f706!2sDESA+TERARA!5e0!3m2!1sid!2sid!4v1522832436151" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+		</div>
+	</div>
+</div>
+
+
+  <footer class="page-footer  deep-orange darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">About Me</h5>
+                <p class="grey-text text-lighten-4">Nama : Lalu Bayu Alami</p>
+                <p>Nim : 1510520165</p>
+                <p>Kelas : C</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">MY Contak</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Github</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facbook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Wa 085-333-288-863</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Youtube</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container center">
+            © 2018 Copyright Bayu Lalu
+            </div>
+          </div>
+        </footer>
+<script src="/js/JQuery.js"></script>
 <script src="/js/materialize.min.js"></script>
+<script src="/js/typed.min.js"></script>
+<script type="text/javascript">
+	 var typed = new Typed('#baca', {
+    strings: ['Membaca Adalah Guru Yang Terbaik ...'],
+    // stringsElement:'#p1',
+    typeSpeed: 70,
+    backSpeed: 120,
+    fadeOut: false,
+    loop: true,
+    // attr: 'placeholder',
+    
+    // bindInputFocusEvents: true,
+  });
+
+	 	$(document).ready(function(){
+		 	 $(".dropdown-trigger").dropdown();
+
+	 	});
+
+	 	$(window).scroll(function(){
+	    	if ($(window).scrollTop() > 150) {
+	    		$('.nav-menu').css({
+	    			'transition': 'display 10s',
+	    			'display':'block',
+
+	    		});
+	    	
+	    	}else{
+	    		$('.nav-menu').css({
+	    			'transition': 'display 10s',
+	    			'display':'none',
+	    		});
+	    	}
+	    	// konten
+	    	// if ($(window).scrollTop() > 200) {
+	    	// 	 $('.pp').addClass('Ani-fto');
+	    	// 	 $('.Konten-pp').addClass('Ani-About');
+    		// }
+	    });
+
+</script>
 </body>
 </html>
