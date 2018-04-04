74d4fad9b8a2b140dbb7e8d9b16b493261185c8e
Bayu lalu <bayulalu3@gmail.com>
2018-04-04 11:20:22 +0700

    home

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Book</title>
+</head>
+<link rel="stylesheet" type="text/css" href="/css/materialize.min.css">
+<link rel="stylesheet" type="text/css" href="/css/home.css">
+<body>
+	<div id="nav-bg">
+		<div class="row">
+			<div class="col s6">
+				<img src="img/nav.png">
+			 </div>
+
+		 <div  class="col s6 ">
+			<a href="" class="waves-effect waves-ligh right btn novel">Novel</a>
+			<a href="" class=" waves-effect waves-ligh right btn home">Home</a>
+		</div>
+		
+	</div>
+
+	<div class="container">
+		<div>
+			{{-- <h3>Membaca Adalah Yang Guru Terbaik</h3> --}}
+			<h3>Ayo Membaca</h3>
+			<p>Ilmu Adalah Insevestasi Berharga Untuk Masa Depan</p>
+		</div>
+		
+	</div>
+
+<script src="/js/materialize.min.js"></script>
+</body>
+</html>
